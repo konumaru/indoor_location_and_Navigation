c1e531859736327fcf35bdd95d23220387b225cd
konumaru <konumaru1022@gmail.com>
2021-03-26 16:42:11 +0900

    [update] print nunique of path.

--- src/split_fold.py
+++ src/split_fold.py
@@ -14,8 +14,14 @@ def main():
     path = waypoint[:, 2]
     cv = GroupKFold(n_splits=5)
     for n_fold, (train_idx, valid_idx) in enumerate(cv.split(waypoint, groups=path)):
+        print(f"Fold {n_fold:>02}")
+
+        train_unique = np.unique(waypoint[train_idx, 2])
+        valid_unique = np.unique(waypoint[valid_idx, 2])
+
+        print(f"\tTrain size: {len(train_idx)} Valid size: {len(valid_idx)}")
         print(
-            f"Fold {n_fold:>02} Train size: {len(train_idx)} Valid size: {len(valid_idx)}"
+            f"\tTrain nunique: {len(train_unique)} Valid nunique: {len(valid_unique)}"
         )
 
         np.save(f"../data/fold/fold{n_fold:>02}_waypoint.npy", waypoint[valid_idx])
