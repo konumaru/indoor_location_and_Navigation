55230424ff1b4a40809532f41cc76138fa54e958
konumaru <konumaru1022@gmail.com>
2021-03-27 00:41:58 +0900

    [update] add label encode process.

--- src/extract_raw_data.py
+++ src/extract_raw_data.py
@@ -11,8 +11,7 @@ from typing import Dict
 import config
 
 from utils import timer
-
-# ==== waypoint ===
+from utils import load_pickle, dump_pickle
 
 
 def get_data_from_pathtxt(
@@ -41,6 +40,9 @@ def get_data_from_pathtxt(
     return data
 
 
+# ==== waypoint ===
+
+
 def get_waypoint_in_parallel(
     src_dir: str, data_type: str, is_join_ids: bool = False
 ) -> np.ndarray:
@@ -53,6 +55,18 @@ def get_waypoint_in_parallel(
     # Data columns is (site, floor, path. timestamp, x, y).
     data = np.concatenate(data, axis=0)
     return data
+
+
+def get_test_waypoint():
+    waypoint = pd.read_csv("../data/raw/sample_submission.csv")
+    waypoint[["site", "path", "timestamp"]] = waypoint["site_path_timestamp"].str.split(
+        "_", expand=True
+    )
+    waypoint.drop(["site_path_timestamp"], axis=1, inplace=True)
+
+    waypoint = waypoint[["site", "floor", "path", "timestamp", "x", "y"]].astype(str)
+    waypoint = waypoint.to_numpy()
+    return waypoint
 
 
 # === Wifi ===
@@ -82,7 +96,8 @@ def get_wifi_from_waypoints(waypoint, max_len=100):
     return data
 
 
-def get_wifi_from_waypoints_in_parallel(waypoints: np.ndarray) -> np.ndarray:
+def get_wifi_from_waypoints_in_parallel() -> np.ndarray:
+    waypoints = np.load("../data/working/train_waypoint.npy")
     data = joblib.Parallel(n_jobs=-1)(
         joblib.delayed(get_wifi_from_waypoints)(waypoint)
         for waypoint in track(waypoints)
@@ -91,18 +106,103 @@ def get_wifi_from_waypoints_in_parallel(waypoints: np.ndarray) -> np.ndarray:
     return data
 
 
+def get_test_wifi_from_waypoints(
+    waypoint: np.ndarray, max_len: int = 100
+) -> np.ndarray:
+    (site, floor, path, timestamp, x, y) = waypoint
+    path_filepath = pathlib.Path(f"../data/raw/test/{path}.txt")
+    wifi = get_data_from_pathtxt(path_filepath, "TYPE_WIFI")
+
+    extract_idx = [2, 3, 4]
+    data = np.full((len(extract_idx), max_len), np.nan, dtype="<U40")
+
+    if len(wifi) > 0:
+        ts_diff = wifi[:, 0].astype("int64") - int(timestamp)
+        ts_diff_min = np.abs(np.min(ts_diff))
+        # Extract latest values, except feature information.
+        wifi = wifi[(ts_diff <= ts_diff_min)]
+        # Extract columns of (bssid, rssi, frequency).
+        wifi = wifi[:, extract_idx]
+        # Sort values by rssi.
+        sort_idx = np.argsort(wifi[:, 1])
+        wifi = wifi[sort_idx]
+
+        data = np.full((len(extract_idx), max_len), np.nan, dtype="<U40")
+        data[:, : wifi.T.shape[1]] = wifi.T[:, :max_len]
+    return data
+
+
+def get_test_wifi_from_waypoints_in_parallel() -> None:
+    waypoints = np.load("../data/working/test_waypoint.npy", allow_pickle=True)
+    data = joblib.Parallel(n_jobs=-1)(
+        joblib.delayed(get_test_wifi_from_waypoints)(waypoint)
+        for waypoint in track(waypoints)
+    )
+    data = np.array(data)
+    return data
+
+
+# === Create map for label encode ===
+
+
+def dump_wifi_bssid_map():
+    wifi_train = np.load("../data/working/train_wifi_features.npy")
+    wifi_test = np.load("../data/working/test_wifi_features.npy")
+    # wifi columns is (bssid, rssi, frequency).
+    wifi = np.concatenate([wifi_train, wifi_test], axis=0)
+
+    bssid_uniques = np.unique(np.ravel(wifi[:, 0]))
+    bssid_map = {bssid: str(i) for i, bssid in enumerate(bssid_uniques)}
+    dump_pickle("../data/working/bssid_map.pkl", bssid_map)
+
+
+def encode_bssid(wifi_feature: np.ndarray) -> np.ndarray:
+    bssid_map = load_pickle("../data/working/bssid_map.pkl")
+    for i, bssids in enumerate(track(wifi_feature[:, 0], total=len(wifi_feature))):
+        wifi_feature[i, 0] = [bssid_map[bssid] for bssid in bssids]
+    return wifi_feature
+
+
 def main():
     src_dir = pathlib.Path("../data/raw/train/")
 
-    print("Processing waypoint of train ...")
-    with timer("Get waypoint of train"):
-        waypoints = get_waypoint_in_parallel(src_dir, "TYPE_WAYPOINT", is_join_ids=True)
-    np.save("../data/working/train_waypoint.npy", waypoints)
+    # TODO:
+    # wifi にある bssid の label encoding 用の map を作成する
 
-    print("Processing wifi of train ...")
-    with timer("Get wifi of train"):
-        wifi_features = get_wifi_from_waypoints_in_parallel(waypoints)
-    np.save("../data/working/train_wifi_features.npy", wifi_features)
+    # Convert from txt to np.ndarray and save as npy.
+
+    # waypints
+    print("Processing waypoint ...")
+    # with timer("Get waypoint of train"):
+    #     waypoints = get_waypoint_in_parallel(src_dir, "TYPE_WAYPOINT", is_join_ids=True)
+    # np.save("../data/working/train_waypoint.npy", waypoints)
+    # with timer("Get waypoint of test"):
+    #     waypoints = get_test_waypoint()
+    # np.save("../data/working/test_waypoint.npy", waypoints)
+
+    # wifi
+    print("Processing wifi ...")
+    # with timer("Get wifi of train"):
+    #     wifi_features = get_wifi_from_waypoints_in_parallel()
+    # np.save("../data/working/train_wifi_features.npy", wifi_features)
+    # with timer("Get wifi of test"):
+    #     wifi_features = get_test_wifi_from_waypoints_in_parallel()
+    # np.save("../data/working/test_wifi_features.npy", wifi_features)
+
+    # Label encode
+    # bssid of wifi
+    print("Processing label encoding ...")
+    dump_wifi_bssid_map()
+
+    with timer("Encode bssid of wifi of train"):
+        wifi_feature = np.load("../data/working/train_wifi_features.npy")
+        wifi_feature = encode_bssid(wifi_feature)
+    np.save("../data/working/encoded_train_wifi_features.npy", wifi_feature)
+
+    with timer("Encode bssid of wifi of test"):
+        wifi_feature = np.load("../data/working/test_wifi_features.npy")
+        wifi_feature = encode_bssid(wifi_feature)
+    np.save("../data/working/encoded_test_wifi_features.npy", wifi_feature)
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,9 @@
 import os
 import time
+import pickle
 import datetime
 from contextlib import contextmanager
+from typing import List, Any
 
 
 @contextmanager
@@ -10,3 +12,17 @@ def timer(name):
     yield
     druration_time = str(datetime.timedelta(seconds=time.time() - start_time))[:7]
     print(f"[{name}] done in {druration_time}\n")
+
+
+def load_pickle(filepath: str, verbose: bool = True):
+    if verbose:
+        print(f"Load pickle from {filepath}")
+    with open(filepath, "rb") as file:
+        return pickle.load(file)
+
+
+def dump_pickle(filepath: str, data: Any, verbose: bool = True):
+    if verbose:
+        print(f"Dump pickle to {filepath}")
+    with open(filepath, "wb") as file:
+        pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
