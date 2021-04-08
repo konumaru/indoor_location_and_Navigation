7a9f36743c5866009c3c1950332393c63d274d44
konumaru <konumaru1022@gmail.com>
2021-04-08 19:12:59 +0900

    [update] num split = 3

--- src/split_fold.py
+++ src/split_fold.py
@@ -1,6 +1,6 @@
 import pathlib
 import numpy as np
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, train_test_split
 from sklearn.model_selection import GroupKFold, GroupShuffleSplit
 
 from utils import timer
@@ -14,22 +14,19 @@ def main():
     ids = load_pickle("../data/preprocessing/train_ids.pkl")
 
     path = ids[:, 2]
-    cv = GroupKFold(n_splits=5)
+    cv = GroupKFold(n_splits=3)
     for n_fold, (train_idx, test_idx) in enumerate(cv.split(ids, groups=path)):
         print(f"Fold {n_fold:>02}")
 
-        cv = GroupShuffleSplit(n_splits=1, train_size=0.8, random_state=config.SEED)
-        for _train_idx, _valid_idx in cv.split(
-            ids[train_idx], groups=ids[train_idx, 2]
-        ):
-            train_unique = np.unique(ids[_train_idx, 2])
-            valid_unique = np.unique(ids[_valid_idx, 2])
+        valid_idx, test_idx = train_test_split(test_idx, test_size=0.5)
 
+        train_unique = np.unique(ids[train_idx, 2])
+        valid_unique = np.unique(ids[valid_idx, 2])
         test_unique = np.unique(ids[test_idx, 2])
 
         print(
-            f"\tTrain size: {len(_train_idx)}",
-            f"Valid size: {len(_valid_idx)}",
+            f"\tTrain size: {len(train_idx)}",
+            f"Valid size: {len(valid_idx)}",
             f"Test size: {len(test_idx)}",
         )
         print(
@@ -38,8 +35,8 @@ def main():
             f"Test nunique: {len(test_unique)}",
         )
 
-        np.save(f"../data/fold/fold{n_fold:>02}_train_idx.npy", _train_idx)
-        np.save(f"../data/fold/fold{n_fold:>02}_valid_idx.npy", _valid_idx)
+        np.save(f"../data/fold/fold{n_fold:>02}_train_idx.npy", train_idx)
+        np.save(f"../data/fold/fold{n_fold:>02}_valid_idx.npy", valid_idx)
         np.save(f"../data/fold/fold{n_fold:>02}_test_idx.npy", test_idx)
 
 
